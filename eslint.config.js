7a43847f482563225ce907cf118ec51de5dd8553
Jérémy Amand <jeremy.amand@proton.me>
2026-02-01 00:50:49 +0100

    chore: Add eslint typescript linting

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,12 @@
 import eslintPluginAstro from 'eslint-plugin-astro';
 import jsxA11y from 'eslint-plugin-jsx-a11y';
+import tseslint from 'typescript-eslint';
 
 export default [
+	// TypeScript support
+	...tseslint.configs.recommended,
+
+	// Astro support
 	...eslintPluginAstro.configs.recommended,
 
 	// Accessibility rules for JSX/Astro
